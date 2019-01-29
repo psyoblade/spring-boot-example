32ed8a1f65775c992c758320ad84d352f67cbcdb
psyoblade <park.suhyuk@gmail.com>
2019-01-29 23:50:59 +0900

    Add unittest

---
+++ src/test/java/com/ncsoft/dataplatform/dummies/springboot/datasource/DataSourceTest.java
@@ -0,0 +1,28 @@
+package com.ncsoft.dataplatform.dummies.springboot.datasource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/test/resources/datasource.xml"})
+public class DataSourceTest {
+
+    @Inject
+    private DataSource ds;
+
+    @Test
+    public void testConnection() throws Exception {
+        try(Connection conn = ds.getConnection()) {
+            System.out.println(conn);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
